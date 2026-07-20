396984880a67edb10dc8837af5a1a524c7fb9a8e
pratham singh raghuwanshi <prathamraghuwanshi640@gmail.com>
2026-07-20 22:47:47 +0530

    Time: 0 ms (100%), Space: 11.9 MB (11.77%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,39 +1,43 @@
 class Solution {
 public:
-    void sortColors(vector<int>& arr) {
-        int zero = 0;
-        int one = 0;
-        int two = 0;
+    
+void sortColors(vector<int>& arr) {
+        vector<int> zero;
+        vector<int> one;
+        vector<int> two;
+        
         vector<int> finalarr;
-        int n = arr.size();
 
+        int n = arr.size();
         for(int i=0 ; i<n ; i++){
             if(arr[i] == 0){
-                zero++;
+                zero.push_back(arr[i]);
             }else if(arr[i] == 1){
-                one++;
+                one.push_back(arr[i]);
             }else{
-                two++;
+                two.push_back(arr[i]);
             }
         }
 
-        for(int i=0 ; i<zero ; i++){
-            finalarr.push_back(0);
+        for(int i=0 ; i<zero.size() ; i++){
+            finalarr.push_back(zero[i]);
         }
-        for(int i=0 ; i<one ; i++){
-            finalarr.push_back(1);
+
+        for(int i=0 ; i<one.size() ; i++){
+            finalarr.push_back(one[i]);
         }
-        for(int i=0 ; i<two ; i++){
-            finalarr.push_back(2);
+
+        for(int i=0 ; i<two.size() ; i++){
+            finalarr.push_back(two[i]);
         }
 
         arr = finalarr;
-        
     }
 };
 
 
-// brute force
+// BRUTE FORCE
+
 // void sortColors(vector<int>& arr) {
 //         vector<int> zero;
 //         vector<int> one;
@@ -66,3 +70,38 @@ public:
 
 //         arr = finalarr;
 //     }
+
+
+// MORE OPTIMISED
+
+// void sortColors(vector<int>& arr) {
+//         int zero = 0;
+//         int one = 0;
+//         int two = 0;
+//         vector<int> finalarr;
+//         int n = arr.size();
+
+//         for(int i=0 ; i<n ; i++){
+//             if(arr[i] == 0){
+//                 zero++;
+//             }else if(arr[i] == 1){
+//                 one++;
+//             }else{
+//                 two++;
+//             }
+//         }
+
+//         for(int i=0 ; i<zero ; i++){
+//             finalarr.push_back(0);
+//         }
+//         for(int i=0 ; i<one ; i++){
+//             finalarr.push_back(1);
+//         }
+//         for(int i=0 ; i<two ; i++){
+//             finalarr.push_back(2);
+//         }
+
+//         arr = finalarr;
+        
+//     }
+
